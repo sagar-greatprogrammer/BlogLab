4141239131bb1d959357671dd30c86375218febf
daigo menejis <dmenejis@deqode.com>
2022-10-29 15:17:19 +0530

    completed repositories.

---
+++ BlogLab.Models/Photo/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Photo
+{
+    public class Photo
+    {
+        public int PhotoId { get; set; }
+        public int ApplicationUserId { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public static implicit operator Photo(List<Photo> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
