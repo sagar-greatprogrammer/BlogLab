d78a981250e038311b7bf542fe9ac3d4d7721049
daigo menejis <dmenejis@deqode.com>
2022-10-31 09:52:08 +0530

    services and controllers created

--- BlogLab.Models/Photo/Photo.cs
+++ BlogLab.Models/Photo/Photo.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace BlogLab.Models.Photo
 {
-    public class Photo
+    public class Photo : PhotoCreate
     {
         public int PhotoId { get; set; }
         public int ApplicationUserId { get; set; }
         public DateTime PublishDate { get; set; }
         public DateTime UpdateDate { get; set; }
 
-        public static implicit operator Photo(List<Photo> v)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
